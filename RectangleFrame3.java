ad29da902ade26ae0987712fc2f2b288fad6c308
glin1 <samlin203@yahoo.com>
2015-05-17 06:41:58 -0700

    Update RectangleFrame3.java

--- RectangleFrame3.java
+++ RectangleFrame3.java
@@ -1,46 +1,17 @@
 import javax.swing.JFrame;
-import java.awt.Image;
+
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.Graphics;
-import java.awt.Dimension;
-import javax.swing.ImageIcon;
 
 /**                         
 This frame contains a moving rectangle.
  */
 public class RectangleFrame3 extends JFrame
 {
-    private Image background;
-    private static final int FRAME_WIDTH = 800;
-    private static final int FRAME_HEIGHT = 815;
+    private static final int FRAME_WIDTH = 300;
+    private static final int FRAME_HEIGHT = 400;
 
     private RectangleComponent3 scene;
-    
-    
-    private void initBoard() 
-    {
-        
-        loadImage();
-        
-        int w = 1000;
-        int h =  1000;
-        setPreferredSize(new Dimension(w, h));        
-    }
-    
-    
-    private void loadImage() {
-        
-        ImageIcon ii = new ImageIcon("Background.jpg");
-        background = ii.getImage();  
-        
-    }
-
-    
-    public void paintComponent(Graphics g) {
-
-        g.drawImage(background, 0, 0, null);
-    }
 
     class FrameWindowListener extends WindowAdapter
     {
@@ -54,7 +25,6 @@ public class RectangleFrame3 extends JFrame
     {
         scene = new RectangleComponent3();
         add(scene);
-        initBoard();
 
         this.addWindowListener(new FrameWindowListener());
 
@@ -64,3 +34,4 @@ public class RectangleFrame3 extends JFrame
         this.setVisible(true);
     }
 } 
+
